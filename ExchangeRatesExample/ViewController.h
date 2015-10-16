ebf5997491737e93bb787a3a062eb34a512debd7
shilinartem <shilinartem@gmail.com>
2015-10-16 23:12:07 +0300

    create app

---
+++ ExchangeRatesExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExchangeRatesExample
+//
+//  Created by Artem Shilin on 16.10.15.
+//  Copyright (c) 2015 Artem Shilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ExchangeRatesExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExchangeRatesExample
+//
+//  Created by Artem Shilin on 16.10.15.
+//  Copyright (c) 2015 Artem Shilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
